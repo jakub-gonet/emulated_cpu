d666e53cdcf1befc2292173d1054765cfbf9994f
Jakub Gonet <jakub.gonet0@gmail.com>
2017-12-05 23:39:41 +0100

    Add isStopped() to CU, conditional jump OP codes and new construtor

--- src/main/java/emulated_cpu/cpu/cu/CU.java
+++ src/main/java/emulated_cpu/cpu/cu/CU.java
@@ -3,6 +3,8 @@ package emulated_cpu.cpu.cu;
 import emulated_cpu.Arguments;
 import emulated_cpu.OpCode;
 import emulated_cpu.OperatingUnit;
+import emulated_cpu.cpu.alu.StatusRegister;
+import emulated_cpu.cpu.memory.Memory;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -10,6 +12,8 @@ import java.util.Arrays;
 public class CU implements OperatingUnit {
     public int instructionPointer = 0;
     private boolean stopped = false;
+    private StatusRegister statusRegister;
+    private Memory memory = Memory.getInstance();
 
     private final ArrayList<OpCode> CU_OP_CODES = new ArrayList<>(Arrays.asList(
         new OpCode((x, y) -> null, 0),  //NOP
@@ -19,10 +23,65 @@ public class CU implements OperatingUnit {
         }, 0),                          //HLT
         new OpCode((x, y) -> y, 2),     //MOV
         new OpCode((x, y) -> {                         //JMP
-            instructionPointer = y;
+            if (x < 0 || x >= memory.getMemory().size())
+                throw new IllegalArgumentException("Jumping out bound of memory");
+            instructionPointer = x;
+            return null;
+        }, 1),
+        new OpCode((x, y) -> {                         //JE
+            if (x < 0 || x >= memory.getMemory().size())
+                throw new IllegalArgumentException("Jumping out bound of memory");
+            if (statusRegister.getZeroFlagState())
+                instructionPointer = x;
+            return null;
+        }, 1),
+        new OpCode((x, y) -> {                         //JNE
+            if (x < 0 || x >= memory.getMemory().size())
+                throw new IllegalArgumentException("Jumping out bound of memory");
+            if (!statusRegister.getZeroFlagState())
+                instructionPointer = x;
+            return null;
+        }, 1),
+        new OpCode((x, y) -> {                         //JL
+            if (x < 0 || x >= memory.getMemory().size())
+                throw new IllegalArgumentException("Jumping out bound of memory");
+            if (statusRegister.getNegativeFlagState())
+                instructionPointer = x;
+            return null;
+        }, 1),
+        new OpCode((x, y) -> {                         //JLE
+            if (x < 0 || x >= memory.getMemory().size())
+                throw new IllegalArgumentException("Jumping out bound of memory");
+            if (statusRegister.getZeroFlagState()
+                || statusRegister.getNegativeFlagState())
+                instructionPointer = x;
+            return null;
+        }, 1),
+        new OpCode((x, y) -> {                         //JG
+            if (x < 0 || x >= memory.getMemory().size())
+                throw new IllegalArgumentException("Jumping out bound of memory");
+            if (statusRegister.getCarryFlagState())
+                instructionPointer = x;
+            return null;
+        }, 1),
+        new OpCode((x, y) -> {                         //JGE
+            if (x < 0 || x >= memory.getMemory().size())
+                throw new IllegalArgumentException("Jumping out bound of memory");
+            if (statusRegister.getZeroFlagState()
+                || statusRegister.getCarryFlagState())
+                instructionPointer = x;
             return null;
         }, 1)
     ));
+
+    /**
+     * Creates new ControlUnit with specified StatusRegister (to be read by OP codes).
+     *
+     * @param statusRegister StatusRegister which is checked
+     */
+    public CU(StatusRegister statusRegister) {
+        this.statusRegister = statusRegister;
+    }
 
     /**
      * Execute CU OP code.
@@ -60,5 +119,9 @@ public class CU implements OperatingUnit {
     @Override
     public ArrayList<OpCode> getOpCodes() {
         return CU_OP_CODES;
+    }
+
+    public boolean isStopped() {
+        return stopped;
     }
 }
