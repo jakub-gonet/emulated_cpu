661cf8c1c542b8e107af9a3585483a7dff736dba
Jakub Gonet <jakub.gonet0@gmail.com>
2017-12-05 08:26:47 +0100

    Simplify Arguments class
    Simplified Arguments class to only contain 2 arguments and argument counting method.

--- src/main/java/emulated_cpu/Arguments.java
+++ src/main/java/emulated_cpu/Arguments.java
@@ -1,16 +1,23 @@
 package emulated_cpu;
 
-import emulated_cpu.cpu.memory.IOInterface;
-
 public class Arguments {
+
     public Integer arg1, arg2;
-    public IOInterface firstAddressType, secondAddressType;
+
+    public Arguments(Integer arg1, Integer arg2) {
+        this.arg1 = arg1;
+        this.arg2 = arg2;
+    }
+
+    public Arguments(Integer arg1) {
+        this(arg1, null);
+    }
+
 
     public int getArgumentsCount() {
-        int argumentCount = 0;
-        if (arg1 != null) argumentCount++;
-        if (arg2 != null) argumentCount++;
-
-        return argumentCount;
+        int count = 0;
+        if (arg1 != null) count++;
+        if (arg2 != null) count++;
+        return count;
     }
 }
