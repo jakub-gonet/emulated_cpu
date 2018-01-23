49ce10c68c3e2c7615d4c7b8e9d2cbc5b45b5e2d
Jakub Gonet <jakub.gonet0@gmail.com>
2018-01-23 21:10:02 +0100

    Fix Stack's code after git manipulation
    Don't blame me, my Git-fu is not strong enough yet

--- src/main/java/emulated_cpu/cpu/memory/Stack.java
+++ src/main/java/emulated_cpu/cpu/memory/Stack.java
@@ -1,34 +1,58 @@
 package emulated_cpu.cpu.memory;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.util.Collections;
 
 public class Stack {
+    private Logger logger = LogManager.getLogger(Memory.class);
+
     private Memory memory = Memory.getInstance();
+    private int size;
+    private static final int defaultStackSize = 16;
 
     private int stackPointer;
 
-    Stack(int size) {
+    public Stack(int size) {
         if (size < 0)
             throw new IllegalArgumentException("Stack's size can't be negative");
-        memory.getMemory()
-              .addAll(Collections.nCopies(size, 0));
 
-        stackPointer = memory.getMemory()
-                             .size();
+        this.size = size;
+        this.memory.getMemory()
+                   .addAll(Collections.nCopies(size, 0));
+        this.stackPointer = memory.getMemory()
+                                  .size() - 1;
     }
 
-    int pop() {
+
+    public Stack() {
+        this(defaultStackSize);
+    }
+
+    public int pop() {
+        checkValidityOfStackPointerPosition(stackPointer + 1);
         return memory.getMemory()
-                     .get(stackPointer++);
+                     .get(++stackPointer);
     }
 
-    void push(int value) {
+    public void push(int value) {
+        checkValidityOfStackPointerPosition(stackPointer - 1);
         memory.getMemory()
-              .set(stackPointer, value);
-        stackPointer--;
+              .set(stackPointer--, value);
     }
 
-    public int getStackPointer() {
-        return stackPointer;
+    private void checkValidityOfStackPointerPosition(int stackPointer) {
+        if (stackPointer < memory.getMemory()
+                                 .size() - size - 1) {
+
+            logger.error("Stack overflow");
+            throw new StackOverflowError("Stack overflow");
+        } else if (stackPointer >= memory.getMemory()
+                                         .size()) {
+
+            logger.error("Stack underflow");
+            throw new StackOverflowError("Stack underflow");
+        }
     }
 }
