2fba349fdc9a64b421572699e663a08ddaf78a41
Jakub Gonet <jakub.gonet0@gmail.com>
2018-09-10 20:49:19 +0200

    Add isStopped(), registers() and restart() in Core

--- src/main/java/cpu/cores/Core.java
+++ src/main/java/cpu/cores/Core.java
@@ -1,12 +1,17 @@
 package cpu.cores;
 
-import cpu.processing.Alu;
-import cpu.processing.Cu;
 import cpu.memory.Memory;
 import cpu.memory.Stack;
 import cpu.memory.registers.Registers;
+import cpu.memory.registers.StatusRegister;
+import cpu.processing.Alu;
+import cpu.processing.Cu;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Core {
+    private Logger logger = LogManager.getLogger(Core.class);
+
     private int PC;
     private Memory memory;
     private Registers registers;
@@ -14,8 +19,26 @@ public class Core {
     private Cu cu;
     private Alu alu;
 
-    Core(Memory memory){
+    Core(Memory memory) {
         this.memory = memory;
     }
 
+    void executeNext() {
+
+    }
+
+    public Registers registers() {
+        return registers;
+    }
+
+    public boolean isStopped() {
+        return registers.statusRegister().state(StatusRegister.StatusFlags.STOPPED);
+    }
+
+    public void restart() {
+        registers.resetRegisters();
+        PC = 0;
+        stack.reset();
+        logger.info("Restarted the CPU");
+    }
 }
