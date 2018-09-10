aec03f61530dd64a6413130c5208e1cd1bbdc3f0
Jakub Gonet <jakub.gonet0@gmail.com>
2018-09-10 21:06:57 +0200

    Add simple implementation of Registers

--- src/main/java/cpu/memory/registers/Registers.java
+++ src/main/java/cpu/memory/registers/Registers.java
@@ -1,4 +1,58 @@
 package cpu.memory.registers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Registers {
+    private List<Register> generalPurpose;
+    private StatusRegister statusRegister;
+
+    /**
+     * Creates new Register object with `count` number of general purpose `Register`s.
+     *
+     * @param count how many registers should be created
+     */
+    public Registers(int count) {
+        generalPurpose = createRegisters(count);
+        statusRegister = new StatusRegister();
+    }
+
+
+    /**
+     * Returns StatusRegister.
+     *
+     * @return StatusRegister object
+     */
+    public StatusRegister statusRegister() {
+        return statusRegister;
+    }
+
+
+    /**
+     * Gets general purpose register by given id.
+     *
+     * @param id id of register
+     * @return Register object
+     */
+    public Register byId(int id) {
+        return generalPurpose.get(id);
+    }
+
+    /**
+     * Gets total register count.
+     *
+     * @return count of general purpose registers
+     */
+    public int size() {
+        return generalPurpose.size();
+    }
+
+    private List<Register> createRegisters(int count) {
+        List<Register> registers = new ArrayList<>();
+
+        for (int i = 0; i < count; i++) {
+            registers.add(new Register());
+        }
+        return registers;
+    }
 }
