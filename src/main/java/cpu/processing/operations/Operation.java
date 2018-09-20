498ce525f8b30b92a97a8b4c81e9f86bd4e78008
Jakub Gonet <jakub.gonet0@gmail.com>
2018-09-21 00:28:49 +0200

    Add Operation c-tor

--- src/main/java/cpu/processing/operations/Operation.java
+++ src/main/java/cpu/processing/operations/Operation.java
@@ -1,11 +1,23 @@
 package cpu.processing.operations;
 
-public class Operation {
-    private OpCode opCode;
-    private int opCodeId;
-    private int IP;
+import cpu.memory.MemoryManager;
+import cpu.memory.Readable;
 
-    private Operation() {
+import java.security.InvalidKeyException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Operation {
+    private final int addrModeLength = 3;
+    private final int addrModeBitFieldLength = (int) Math.pow(2, addrModeLength) - 1;
+    private final int argNumLength = 2;
+    private final int argNumBitFieldLength = (int) Math.pow(2, argNumLength) - 1;
+    private Readable memory;
+    private MemoryManager manager;
+
+    public Operation(Readable memory, MemoryManager manager) {
+        this.manager = manager;
+        this.memory = memory;
     }
 
     public void fetchOpCode(OpCodes opCodes){
