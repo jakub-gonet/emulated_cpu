0ef10417b2f4e1c76a705c8290b38fd818ef3cc7
Jakub Gonet <jakub.gonet0@gmail.com>
2018-09-21 00:28:00 +0200

    Add memory manager class

--- src/main/java/cpu/memory/MemoryManager.java
+++ src/main/java/cpu/memory/MemoryManager.java
@@ -1,4 +1,28 @@
 package cpu.memory;
 
+import java.security.InvalidKeyException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
 public class MemoryManager {
+    private Map<Integer, Device> deviceMapping = new HashMap<>();
+
+    public boolean addDevice(int id, Device device) {
+        if (deviceMapping.get(id) != null) return false;
+
+        deviceMapping.put(id, device);
+        return true;
+    }
+
+    public Readable ReadableDevice(int id) throws InvalidKeyException, IllegalAccessException {
+        Device device = deviceOrThrow(id);
+
+        if (!device.isReadable()) throw new IllegalAccessException("Device with id " + id + " is not Readable");
+        return (Readable) device.self();
+    }
+
+    private Device deviceOrThrow(int id) throws InvalidKeyException {
+        return Optional.ofNullable(deviceMapping.get(id)).orElseThrow(() -> new InvalidKeyException("Device with id " + id + " not found"));
+    }
 }
