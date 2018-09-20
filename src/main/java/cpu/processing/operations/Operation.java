aa7a855d9ee7d972c652efb350e00313170f4ee2
Jakub Gonet <jakub.gonet0@gmail.com>
2018-09-21 00:35:56 +0200

    Add fetch() implementation

--- src/main/java/cpu/processing/operations/Operation.java
+++ src/main/java/cpu/processing/operations/Operation.java
@@ -2,12 +2,15 @@ package cpu.processing.operations;
 
 import cpu.memory.MemoryManager;
 import cpu.memory.Readable;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.security.InvalidKeyException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Operation {
+    private Logger logger = LogManager.getLogger(Operation.class);
     private final int addrModeLength = 3;
     private final int addrModeBitFieldLength = (int) Math.pow(2, addrModeLength) - 1;
     private final int argNumLength = 2;
@@ -20,12 +23,32 @@ public class Operation {
         this.memory = memory;
     }
 
-    public void fetchOpCode(OpCodes opCodes){
-        opCode = opCodes.byId(opCodeId);
+    public int fetch(int currentAddress) throws IllegalStateException {
+        List<Integer> args = new ArrayList<>();
+
+        int opCodeAndAddresses = memory.read(currentAddress++);
+        int argNum = (opCodeAndAddresses >> 2 * addrModeLength) & argNumBitFieldLength;
+
+        for (int i = argNum - 1; i >= 0; i--) {
+            int nextValue = memory.read(currentAddress++);
+            int deviceIdContainingValue = (opCodeAndAddresses >> i * addrModeLength) & addrModeBitFieldLength;
+
+            try {
+                args.add(valueFromDevice(deviceIdContainingValue, nextValue));
+            } catch (InvalidKeyException e) {
+                logger.error("Invalid device id: " + deviceIdContainingValue);
+                throw new IllegalStateException("Invalid device id in argument");
+            } catch (IllegalAccessException e) {
+                logger.error("Device with " + deviceIdContainingValue + " id is not Readable");
+                throw new IllegalStateException("Device not Readable");
+            }
+        }
+
+        return currentAddress;
     }
 
-    public int execute(){
-        return IP;
+    private int valueFromDevice(int deviceId, int address) throws InvalidKeyException, IllegalAccessException {
+        Readable device = manager.ReadableDevice(deviceId);
+        return device.read(address);
     }
-
 }
