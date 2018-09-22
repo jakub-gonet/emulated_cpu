c9c0e150ce2f9290e6400e84b2561db6e8b7801b
Jakub Gonet <jakub.gonet0@gmail.com>
2018-09-22 22:07:30 +0200

    Make Core use MemoryManager instead of Memory

--- src/main/java/cpu/cores/Core.java
+++ src/main/java/cpu/cores/Core.java
@@ -1,13 +1,17 @@
 package cpu.cores;
 
 import cpu.memory.Memory;
+import cpu.memory.MemoryManager;
 import cpu.memory.Stack;
 import cpu.memory.registers.Registers;
 import cpu.memory.registers.StatusRegister;
 import cpu.processing.Alu;
 import cpu.processing.Cu;
+import cpu.processing.operations.Operation;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import java.security.InvalidKeyException;
 
 /**
  * This class resembles a single CPU core.
@@ -17,18 +21,25 @@ public class Core {
 
     private int PC;
     private Memory memory;
+    private MemoryManager memManager;
     private Registers registers;
     private Stack stack;
     private Cu cu;
     private Alu alu;
 
-    /**
-     * Create a new core with `memory` as memory.
-     *
-     * @param memory Memory object
-     */
-    Core(Memory memory) {
-        this.memory = memory;
+    Core(MemoryManager memManager) throws IllegalStateException {
+        this.memManager = memManager;
+        try {
+            this.memory = memManager.readableWritableDevice(0);
+        } catch (InvalidKeyException e) {
+            logger.error("Could not obtain Memory from MemoryManager");
+            throw new IllegalStateException();
+        }
+        registers = new Registers(10);
+        cu = new Cu();
+        alu = new Alu();
+
+        logger.info(memory.rawContent());
     }
 
     void executeNext() {
@@ -50,7 +61,7 @@ public class Core {
     public void restart() {
         registers.resetRegisters();
         PC = 0;
-//        stack.reset();
+        //        stack.reset();
         logger.info("Restarted the CPU");
     }
 }
