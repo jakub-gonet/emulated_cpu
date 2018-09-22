07fe2a239f018dbb98558984edd0377b0c43aa31
Jakub Gonet <jakub.gonet0@gmail.com>
2018-09-22 23:41:23 +0200

    Add Readable-Writable function stubs to Stack

--- src/main/java/cpu/memory/Stack.java
+++ src/main/java/cpu/memory/Stack.java
@@ -1,4 +1,23 @@
 package cpu.memory;
 
-public class Stack {
+public class Stack implements Readable, Writable {
+    @Override
+    public int read(int address) {
+        return 0;
+    }
+
+    @Override
+    public boolean canReadAt(int address) {
+        return false;
+    }
+
+    @Override
+    public void write(int address, int data) {
+
+    }
+
+    @Override
+    public boolean canWriteAt(int address) {
+        return false;
+    }
 }
