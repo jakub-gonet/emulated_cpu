e763f9f288eb3f6ff8c73bd4e8860fa070de714c
Jakub Gonet <jakub.gonet0@gmail.com>
2018-09-22 21:48:10 +0200

    Change structure of MemoryManager to not use two maps
    Now it uses enum Type to ensure that extracted device is with correct type.

--- src/main/java/cpu/memory/MemoryManager.java
+++ src/main/java/cpu/memory/MemoryManager.java
@@ -6,47 +6,61 @@ import java.util.Map;
 import java.util.Optional;
 
 public class MemoryManager {
-    private Map<Integer, Readable> readableDeviceMapping = new HashMap<>();
-    private Map<Integer, Writable> writableDeviceMapping = new HashMap<>();
+    private enum Type {
+        READABLE,
+        WRITABLE,
+        READABLE_WRITABLE
+    }
+
+    private Map<Integer, Map<Type, Object>> deviceMapping = new HashMap<>();
 
     public <T extends Readable & Writable> boolean addReadableWritableDevice(int id, T device) {
-        if (isIdNotUsed(writableDeviceMapping, id) && isIdNotUsed(readableDeviceMapping, id)) {
-            addReadableDevice(id, device);
-            addWritableDevice(id, device);
-            return true;
-        }
-        return false;
+        return addDevice(id, Type.READABLE_WRITABLE, device);
     }
 
     public boolean addReadableDevice(int id, Readable device) {
-        if (isIdNotUsed(readableDeviceMapping, id)) {
-            readableDeviceMapping.put(id, device);
-            return true;
-        }
-        return false;
+        return addDevice(id, Type.READABLE, device);
     }
 
     public boolean addWritableDevice(int id, Writable device) {
-        if (isIdNotUsed(writableDeviceMapping, id)) {
-            writableDeviceMapping.put(id, device);
+        return addDevice(id, Type.WRITABLE, device);
+    }
+
+    public Readable readableDevice(int id) throws InvalidKeyException {
+        return (Readable) deviceByType(deviceWithTypeById(id), Type.READABLE);
+    }
+
+    public Writable writableDevice(int id) throws InvalidKeyException {
+        return (Writable) deviceByType(deviceWithTypeById(id), Type.WRITABLE);
+    }
+
+    public <T extends Writable & Readable> T readableWritableDevice(int id) throws InvalidKeyException {
+        return (T) deviceByType(deviceWithTypeById(id), Type.READABLE_WRITABLE);
+    }
+
+    private Object deviceByType(Map<Type, Object> deviceWithType, Type type) throws InvalidKeyException {
+        return Optional.ofNullable(deviceWithType.get(type))
+                       .orElseThrow(() -> new InvalidKeyException("Device with type" + deviceWithType + " is not " + type));
+    }
+
+    private Map<Type, Object> deviceWithTypeById(int id) throws InvalidKeyException {
+        return Optional.ofNullable(deviceMapping.get(id))
+                       .orElseThrow(() -> new InvalidKeyException("Object with id " + id + " not found"));
+    }
+
+    private boolean addDevice(int id, Type type, Object device) {
+        if (isIdNotUsed(id)) {
+            deviceMapping.put(id, createDeviceMapping(type, device));
             return true;
         }
         return false;
     }
 
-    public Readable readableDevice(int id) throws InvalidKeyException {
-        return (Readable) mappingOrThrow(readableDeviceMapping, id);
+    private boolean isIdNotUsed(int id) {
+        return !deviceMapping.containsKey(id);
     }
 
-    public Writable writableDevice(int id) throws InvalidKeyException {
-        return (Writable) mappingOrThrow(writableDeviceMapping, id);
-    }
-
-    private Object mappingOrThrow(Map mapping, int id) throws InvalidKeyException {
-        return Optional.ofNullable(mapping.get(id)).orElseThrow(() -> new InvalidKeyException("Object with id " + id + " not found in mapping: " + mapping.toString()));
-    }
-
-    private boolean isIdNotUsed(Map mapping, int id) {
-        return !mapping.containsKey(id);
+    private Map<Type, Object> createDeviceMapping(Type type, Object device) {
+        return new HashMap<>(Map.of(type, device));
     }
 }
