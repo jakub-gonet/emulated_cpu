e92e90582c92b1c759cf078c5a8530e28751b4d5
Jakub Gonet <jakub.gonet0@gmail.com>
2018-09-23 10:10:45 +0200

    Add docs to Readable interface

--- src/main/java/cpu/memory/Readable.java
+++ src/main/java/cpu/memory/Readable.java
@@ -1,6 +1,24 @@
 package cpu.memory;
 
+/**
+ * Interface used to mark devices as read only.
+ *
+ * @see Writable
+ */
 public interface Readable {
+    /**
+     * Reads value at given address.
+     *
+     * @param address an address in device memory bounds
+     * @return read value
+     */
     int read(int address);
+
+    /**
+     * Checks if given address can be used for reading.
+     *
+     * @param address an address to check
+     * @return true if can read at address, false otherwise
+     */
     boolean canReadAt(int address);
 }
