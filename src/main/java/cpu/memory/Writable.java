1d6a4d8be9d863e51e91547d0ccf1fb809c2c1d9
Jakub Gonet <jakub.gonet0@gmail.com>
2018-09-23 10:14:50 +0200

    Add docs for Writable interface

--- src/main/java/cpu/memory/Writable.java
+++ src/main/java/cpu/memory/Writable.java
@@ -1,7 +1,24 @@
 package cpu.memory;
 
+/**
+ * Interface used to mark devices as write only.
+ *
+ * @see Readable
+ */
 public interface Writable {
+    /**
+     * Writes the data at given address.
+     *
+     * @param address an address in device memory bounds
+     * @param data    data to write
+     */
     void write(int address, int data);
 
+    /**
+     * Checks if given address can be used for writing.
+     *
+     * @param address an address to check
+     * @return true if can write at address, false otherwise
+     */
     boolean canWriteAt(int address);
 }
