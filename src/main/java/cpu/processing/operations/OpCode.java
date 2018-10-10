7e1c20d82d9f9dcd9b418be798affa3d17f39aff
Jakub Gonet <jakub.gonet0@gmail.com>
2018-10-11 00:48:45 +0200

    Change operation() to applyOperation() in OpCode

--- src/main/java/cpu/processing/operations/OpCode.java
+++ src/main/java/cpu/processing/operations/OpCode.java
@@ -1,10 +1,17 @@
 package cpu.processing.operations;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.function.BinaryOperator;
 
 public class OpCode {
     private BinaryOperator<Integer> operation;
     private int requiredArguments;
+    private Logger logger = LogManager.getLogger(OpCode.class);
 
     public OpCode(BinaryOperator<Integer> operation, int requiredArguments) {
         this.operation = operation;
@@ -15,7 +22,17 @@ public class OpCode {
         return requiredArguments;
     }
 
-    public BinaryOperator<Integer> operation() {
-        return operation;
+    public Integer applyOperation(List<Integer> args) {
+        if (args.size() != requiredArguments) {
+            logger.error("OP code have {} arguments, when {} arguments are required", args.size(), requiredArguments);
+            throw new IllegalStateException("Arguments number not matching");
+        }
+
+        ArrayList<Integer> argsCopy = new ArrayList<>(args);
+        if (argsCopy.size() < 2) {
+            argsCopy.addAll(Collections.nCopies(2 - argsCopy.size(), 0));
+        }
+
+        return operation.apply(argsCopy.get(0), argsCopy.get(1));
     }
 }
