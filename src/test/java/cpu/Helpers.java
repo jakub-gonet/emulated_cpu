ec529b02757baf77bdc16bbb7f474bc43c3250ed
Jakub Gonet <jakub.gonet0@gmail.com>
2018-10-10 23:55:19 +0200

    Make Helper methods public

--- src/test/java/cpu/Helpers.java
+++ src/test/java/cpu/Helpers.java
@@ -1,7 +1,9 @@
+package cpu;
+
 import cpu.processing.operations.InstructionDecoder;
 
 public class Helpers {
-    static int opCode(int num, int argNum, int firstAddrMode, int secondAddrMode) {
+    public static int opCode(int num, int argNum, int firstAddrMode, int secondAddrMode) {
         int addrModeLength = InstructionDecoder.addrModeLength();
         int argNumLength = InstructionDecoder.addrNumLength();
         return num << (addrModeLength * 2 + argNumLength) | argNum << (addrModeLength * 2) | firstAddrMode << addrModeLength | secondAddrMode;
