744f7edb3f4f077411bb187ff2d78a8018fc194a
Jakub Gonet <jakub.gonet0@gmail.com>
2018-10-11 17:46:03 +0200

    Add test case for MemManager

--- src/test/java/cpu/memory/MemoryManagerTest.java
+++ src/test/java/cpu/memory/MemoryManagerTest.java
@@ -1,21 +1,26 @@
 package cpu.memory;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class MemoryManagerTest {
+    private MemoryManager manager;
+    private Memory mem;
+
+    @BeforeEach
+    void setup() {
+        mem = new Memory(1);
+        manager = new MemoryManager(mem);
+    }
+
     @Test
     void canCreateManagerWithDefaultMapping() {
-        Memory mem = new Memory(1);
-        MemoryManager manager = new MemoryManager(mem);
-
         Assertions.assertEquals(mem, manager.readableWritableDevice(1));
     }
 
     @Test
     void canInsertSameDeviceWithDifferentIdsAndTypes() {
-        Memory mem = new Memory(1);
-        MemoryManager manager = new MemoryManager(mem);
         Assertions.assertTrue(manager.addWritableDevice(16, mem));
         Assertions.assertTrue(manager.addReadableDevice(17, mem));
         Assertions.assertTrue(manager.addReadableWritableDevice(18, mem));
@@ -23,16 +28,12 @@ class MemoryManagerTest {
 
     @Test
     void cantInsertDeviceWithExistingId() {
-        Memory mem = new Memory(1);
-        MemoryManager manager = new MemoryManager(mem);
         Assertions.assertTrue(manager.addWritableDevice(16, mem));
         Assertions.assertFalse(manager.addWritableDevice(16, mem));
     }
 
     @Test
     void gettingDeviceTypeByDifferentTypeThrowsAnException() {
-        Memory mem = new Memory(1);
-        MemoryManager manager = new MemoryManager(mem);
         manager.addReadableDevice(16, mem);
         manager.addWritableDevice(17, mem);
 
@@ -42,12 +43,17 @@ class MemoryManagerTest {
 
     @Test
     void canGetDeviceWithSubtype() {
-        Memory mem = new Memory(1);
-        MemoryManager manager = new MemoryManager(mem);
         manager.addReadableWritableDevice(16, mem);
 
         Assertions.assertEquals(mem, manager.readableDevice(16));
         Assertions.assertEquals(mem, manager.writableDevice(16));
+    }
+
+    @Test
+    void cantUseNotRegisteredDevice(){
+        Assertions.assertThrows(IllegalArgumentException.class, () -> manager.writableDevice(16));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> manager.readableDevice(17));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> manager.readableWritableDevice(18));
 
     }
 }
