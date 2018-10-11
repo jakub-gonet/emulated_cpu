93cb56ae9033641a078cbfd0095919ac8aee7fd8
Jakub Gonet <jakub.gonet0@gmail.com>
2018-10-11 20:41:49 +0200

    Add stubs for CU test cases

--- src/test/java/cpu/cu/CuTest.java
+++ src/test/java/cpu/cu/CuTest.java
@@ -8,28 +8,83 @@ import cpu.memory.registers.Registers;
 import cpu.processing.Cu;
 import cpu.processing.operations.Operation;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
 class CuTest {
-    @Test
-    void canExecuteJumpOpCodes() {
-        Memory mem = new Memory(List.of(
-                0,
+    private Memory mem;
+    private MemoryManager manager;
+    private Cu cu;
+    private Operation operation;
+
+    @BeforeEach
+    void setup() {
+        mem = new Memory(List.of(
+                -1,
                 5,
-                Helpers.opCode(3, 1, 0, 0),
-                0
+                Helpers.opCode(0, 0, 0, 0),
+                Helpers.opCode(3, 1, 0, 0), 0,
+                Helpers.opCode(26, 2, 0, 0), 1, 1,
+                Helpers.opCode(4, 1, 0, 0), 0,
+                Helpers.opCode(26, 2, 0, 0), 1, 2,
+                Helpers.opCode(5, 1, 0, 0), 0
         ));
-        MemoryManager manager = new MemoryManager(new MemoryManager(mem), new Registers(8));
-        Cu cu = new Cu(manager, new Stack());
+        manager = new MemoryManager(new MemoryManager(mem), new Registers(8));
+        cu = new Cu(manager, new Stack());
+        operation = new Operation(manager);
+    }
 
-        Operation operation = new Operation(manager);
+    @Test
+    void NOP() {
         int PC = operation.fetch(2);
+        Assertions.assertDoesNotThrow(() -> cu.execute(PC, operation));
+    }
 
-        PC = cu.execute(PC, operation);
+    @Test
+    void HLT() {
 
-        Assertions.assertEquals(0, PC);
+    }
+
+    @Test
+    void JE_JNE() {
+
+    }
+
+    @Test
+    void JL_JLE() {
+
+    }
+
+    @Test
+    void JG_JGE() {
+
+    }
+
+    @Test
+    void PUSH_POP() {
+
+    }
+
+    @Test
+    void CALL_RET() {
+
+    }
+
+    @Test
+    void arithmeticOpCodes() {
+
+    }
+
+    @Test
+    void logicOpCodes() {
+
+    }
+
+    @Test
+    void CMP() {
+
     }
 }
 
