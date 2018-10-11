c6aa3021e45eb98395bc5ff09546598534839747
Jakub Gonet <jakub.gonet0@gmail.com>
2018-10-11 16:48:18 +0200

    Refactor addressing mode test to use @BeforeEach

--- src/test/java/cpu/memory/addressing_modes/AddressFromRegisterTest.java
+++ src/test/java/cpu/memory/addressing_modes/AddressFromRegisterTest.java
@@ -3,17 +3,25 @@ package cpu.memory.addressing_modes;
 import cpu.memory.Memory;
 import cpu.memory.registers.Registers;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
 class AddressFromRegisterTest {
+    private AddressFromRegister addressFromRegister;
+    private Memory mem;
+    private Registers registers;
+
+    @BeforeEach
+    void setup() {
+        mem = new Memory(List.of(5, 10, 15));
+        registers = new Registers(2);
+        addressFromRegister = new AddressFromRegister(mem, registers);
+    }
+
     @Test
     void canReadAndWriteInRegisterBounds() {
-        Memory mem = new Memory(List.of(5, 10, 15));
-        Registers registers = new Registers(2);
-        AddressFromRegister addressFromRegister = new AddressFromRegister(mem, registers);
-
         registers.write(1, 2);
 
         Assertions.assertEquals(15, addressFromRegister.read(1));
@@ -23,10 +31,6 @@ class AddressFromRegisterTest {
 
     @Test
     void cantReadOrWriteOutsideRegisterBounds() {
-        Memory mem = new Memory(List.of(5, 10, 15));
-        Registers registers = new Registers(2);
-        AddressFromRegister addressFromRegister = new AddressFromRegister(mem, registers);
-
         Assertions.assertThrows(IndexOutOfBoundsException.class, () -> addressFromRegister.read(2));
         Assertions.assertThrows(IndexOutOfBoundsException.class, () -> addressFromRegister.write(2, 5));
         Assertions.assertFalse(addressFromRegister.canReadAt(-1));
@@ -35,10 +39,6 @@ class AddressFromRegisterTest {
 
     @Test
     void canReadAndWriteInMemoryBounds() {
-        Memory mem = new Memory(List.of(5, 10, 15));
-        Registers registers = new Registers(2);
-        AddressFromRegister addressFromRegister = new AddressFromRegister(mem, registers);
-
         registers.write(1, 2);
 
         Assertions.assertEquals(15, addressFromRegister.read(1));
@@ -48,10 +48,6 @@ class AddressFromRegisterTest {
 
     @Test
     void cantReadOrWriteOutsideMemoryBounds() {
-        Memory mem = new Memory(List.of(5, 10, 15));
-        Registers registers = new Registers(2);
-        AddressFromRegister addressFromRegister = new AddressFromRegister(mem, registers);
-
         registers.write(1, 5);
 
         Assertions.assertThrows(IndexOutOfBoundsException.class, () -> addressFromRegister.read(1));
